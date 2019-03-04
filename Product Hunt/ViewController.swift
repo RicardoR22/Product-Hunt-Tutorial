ce7ac955d29388d6cbec4c5a32d5a21f9f8a3d87
Ricardo Rodriguez <rr092293@gmail.com>
2019-03-04 12:48:54 -0800

    First Commit

---
+++ Product Hunt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Product Hunt
+//
+//  Created by Ricardo Rodriguez on 3/4/19.
+//  Copyright © 2019 Ricardo Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
