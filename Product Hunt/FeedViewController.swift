1e22d92574617d3525588fd40a2d0709f04223cd
Ricardo Rodriguez <rr092293@gmail.com>
2019-03-04 13:11:00 -0800

    Setup Table View

---
+++ Product Hunt/FeedViewController.swift
@@ -0,0 +1,45 @@
+//
+//  FeedViewController.swift
+//  Product Hunt
+//
+//  Created by Ricardo Rodriguez on 3/4/19.
+//  Copyright © 2019 Ricardo Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    
+    @IBOutlet weak var feedTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        feedTableView.dataSource = self
+        feedTableView.delegate = self
+        
+        
+    }
+
+
+}
+
+// MARK: UITableViewDataSource
+extension FeedViewController: UITableViewDataSource {
+    /// Determines how many cells will be shown on the table view.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    /// Creates and configures each cell.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+
+// MARK: UITableViewDelegate
+extension FeedViewController: UITableViewDelegate {
+    // Code to handle cell events goes here...
+}
