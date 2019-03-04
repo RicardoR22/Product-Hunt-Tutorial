9e81a19e2ba525e18a45a704a3f8b4cb31b5228e
Ricardo Rodriguez <rr092293@gmail.com>
2019-03-04 14:38:50 -0800

    Created Post Mode

---
+++ Product Hunt/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  Product Hunt
+//
+//  Created by Ricardo Rodriguez on 3/4/19.
+//  Copyright © 2019 Ricardo Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    let id: Int
+    let name: String
+    let tagline: String
+    let votesCount: Int
+    let commentsCount: Int
+}
