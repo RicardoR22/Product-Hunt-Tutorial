7eec49118b164ca9ea02cabecb837721786f2e15
Ricardo Rodriguez <rr092293@gmail.com>
2019-03-04 15:16:06 -0800

    Created Custom Class for Posts

---
+++ Product Hunt/PostTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  PostTableViewCell.swift
+//  Product Hunt
+//
+//  Created by Ricardo Rodriguez on 3/4/19.
+//  Copyright © 2019 Ricardo Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentsCountLabel: UILabel!
+    @IBOutlet weak var votesCountLabel: UILabel!
+    @IBOutlet weak var taglineLabel: UILabel!
+    @IBOutlet weak var previewImageView: UIImageView!
+    
+    var post: Post? {
+        didSet {
+            guard let post = post else { return }
+            nameLabel.text = post.name
+            taglineLabel.text = post.tagline
+            commentsCountLabel.text = "Comments: \(post.commentsCount)"
+            votesCountLabel.text = "Votes: \(post.votesCount)"
+            updatePreviewImage()
+        }
+    }
+    
+    func updatePreviewImage() {
+        guard let post = post else { return }
+        previewImageView.image = UIImage(named: "placeholder")
+    }
+    
+}
